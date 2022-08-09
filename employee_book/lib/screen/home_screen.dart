bb56351feff153abf28206bac3da1f8f9c48e275
swativishnu <swativishnu@gmail.com>
2022-08-09 16:23:29 +0530

    Employee book created

---
+++ employee_book/lib/screen/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home screen'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: const [],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.pushNamed(context, '/add_employee');
+        },
+        icon: const Icon(Icons.add),
+        label: const Text("Add Employee"),
+      ),
+    );
+  }
+}
